34b9bda0cf1ac93198564bc074d065b54c849543
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-07-28 20:50:57 +0900

    add_routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root "posts#index"
+  resources :users, only: [:show, :edit, :update]
+  resources :posts, expect: [:index]
 end
