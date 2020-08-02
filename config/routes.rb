13ad8682019eca98ecdcb517413b67480cc74de7
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-08-02 19:54:23 +0900

    add_gem_for_sns_ Login_authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
   root "posts#index"
   resources :users, only: [:show, :edit, :update]
   resources :posts, expect: [:index]
