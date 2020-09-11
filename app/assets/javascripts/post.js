12f7e2faaf8aea5e7d31679b3e0ae13c78e9d270
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-09-11 23:50:48 +0900

    add_tag-it(js)

---
+++ app/assets/javascripts/post.js
@@ -0,0 +1,41 @@
+//---------------サイドバーで切り替え---------------//
+$(document).on("click", '.side__list', function() {
+  $(".side__list").removeClass("active");
+  $(".side__list").css("font-weight", "");
+  $(".side__list").css("background-color", "");
+
+  $(this).addClass("active");
+  $(this).css("font-weight", "bolder");
+  $(this).css("background-color", "gray");
+
+  let index = $('.side li').index(this);  // インデックス番号を取得
+  if (index == 0) {   //キャンバス
+    $(".canvas").css("display", "block");
+    $(".post_user").css("display", "block");
+    $(".memo_box").css("display", "none");
+  } else if (index == 1) {  // ノート
+    $(".memo_box").css("display", "block");
+    $(".canvas").css("display", "none");
+    $(".post_user").css("display", "none");
+  } else {  // 画像
+    $(".canvas").css("display", "none");
+    $(".post_user").css("display", "none");
+    $(".memo_box").css("display", "none");
+  }
+});
+
+//-----------------------タグ入力-----------------------//
+$(document).ready(function() {
+  let tag_count = 10 - $(".tagit-choice").length
+  $(".post_form__body__form__text.tag").tagit({
+    tagLimit:10,   // タグの最大数
+    singleField: true,   // タグの一意性
+    availableTags: ['php', 'ruby', 'react', 'reactNative', 'laravel']
+  });
+})
+
+$(document).on("keyup", '.tagit', function() {
+  let tag_count = 10 - $(".tagit-choice").length
+  $(".ui-widget-content.ui-autocomplete-input").attr(
+  'placeholder','あと' + tag_count + '個登録できます');
+})
