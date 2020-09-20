0d8dfc783ae0bddccf6285f6794346e2fd36c944
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-09-20 17:00:40 +0900

    create_sns_credential_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200920065000) do
+ActiveRecord::Schema.define(version: 20200920075333) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",                  null: false
