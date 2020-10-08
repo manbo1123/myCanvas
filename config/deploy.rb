12a0a610e1ae537b9faa4aa3f1685d73eef5cab3
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-10-08 12:50:53 +0900

    edit_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"   # capistranoのバージョンを固定
 
-set :application, "my_canvas"  # Capistranoのログの表示用
+set :application, "myCanvas"  # Capistranoのログの表示用
 set :repo_url, "git@github.com:manbo1123/myCanvas.git"    # どのリポジトリからアプリをpullするかを指定
 
 # バージョンが変わっても共通で参照するディレクトリを指定
@@ -16,6 +16,8 @@ set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/myCanvas.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }        # プロセス番号を記載したファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }  # Unicornの設定ファイルの場所
 set :keep_releases, 5
+
+set :linked_files, %w{ config/secrets.yml }   # secrets.yml用のシンボリックリンクを追加
 
 # デプロイ終了後、Unicornを再起動する記述
 after 'deploy:publishing', 'deploy:restart'
